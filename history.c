f099306a8a9dac1ee6f10746ff50b0ab4f73c63a
KanishAnand <therealkanish@gmail.com>
2019-09-05 19:17:30 +0530

    formatted code

--- history.c
+++ history.c
@@ -1,27 +1,24 @@
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <sys/stat.h>
-#include <sys/utsname.h>
-#include <readline/readline.h>
-#include <readline/history.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <stdlib.h>
 #include <dirent.h>
+#include <fcntl.h>
+#include <readline/history.h>
+#include <readline/readline.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/utsname.h>
+#include <sys/wait.h>
+#include <unistd.h>
 #include "main.h"
 int cnt = 0;
 
-void push(char *in, char **qu)
-{
-
+void push(char *in, char **qu) {
     qu[cnt] = in;
     cnt++;
 }
 
-void history(char *rd)
-{
+void history(char *rd) {
     cnt = 0;
 
     char **qu = (char **)malloc(sizeof(char *) * 500);
@@ -35,46 +32,38 @@ void history(char *rd)
     read(fd, buff, 1000);
     char **parts = (char **)malloc(50 * sizeof(char *));
     parts[0] = strtok(buff, "\n");
-    if (parts[0] != NULL)
-    {
+    if (parts[0] != NULL) {
         push(parts[0], qu);
     }
     int i = 0;
-    while (parts[i] != NULL)
-    {
+    while (parts[i] != NULL) {
         ++i;
         parts[i] = strtok(NULL, "\n");
-        if (parts[i] != NULL)
-        {
+        if (parts[i] != NULL) {
             push(parts[i], qu);
         }
     }
     --i;
 
-    if (strcmp(parts[i], rd) != 0)
-    {
+    if (strcmp(parts[i], rd) != 0) {
         push(rd, qu);
     }
     close(fd);
     fd = open(st, O_WRONLY | O_TRUNC, 0677);
     int p = 0;
-    if (cnt - 20 > 0)
-    {
+    if (cnt - 20 > 0) {
         p = cnt - 20;
     }
-    for (int i = p; i < cnt; i++)
-    {
+    for (int i = p; i < cnt; i++) {
         write(fd, qu[i], strlen(qu[i]));
-        if (i < cnt - 1)
-        {
+        if (i < cnt - 1) {
             write(fd, "\n", 1);
         }
     }
     close(fd);
 }
 
-void showhistory()
-{
+void showhistory() {
     char **qu = (char **)malloc(500);
 
     char *st = (char *)malloc(1000);
@@ -89,24 +78,20 @@ void showhistory()
 
     char *ptr = strtok(buff, "\n");
     cnt = 0;
-    if (ptr)
-    {
+    if (ptr) {
         qu[cnt++] = ptr;
-        while ((ptr = strtok(NULL, "\n")))
-        {
+        while ((ptr = strtok(NULL, "\n"))) {
             qu[cnt++] = ptr;
         }
     }
 
     close(fd);
     int p = 0;
-    if (cnt > 10)
-    {
+    if (cnt > 10) {
         p = cnt - 10;
     }
 
-    for (int i = p; i < cnt - 5; i++)
-    {
+    for (int i = p; i < cnt - 5; i++) {
         printf("%s\n", qu[i]);
     }
 }
--- redirection.c
+++ redirection.c
@@ -1,32 +1,30 @@
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <sys/stat.h>
-#include <sys/utsname.h>
-#include <readline/readline.h>
-#include <readline/history.h>
-#include <unistd.h>
-#include <stdlib.h>
 #include <dirent.h>
+#include <readline/history.h>
+#include <readline/readline.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-#include "wait_input.h"
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/utsname.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include "execute_command.h"
+#include "history.h"
 #include "init_shell.h"
 #include "ls_implement.h"
-#include "execute_command.h"
 #include "main.h"
-#include "pinfo.h"
-#include "history.h"
 #include "nightswatch.h"
+#include "pinfo.h"
 #include "system_commands.h"
+#include "wait_input.h"
 
-void redirection(char *args)
-{
+void redirection(char *args) {
     char **parts = (char **)malloc(400);
     parts[0] = strtok(args, "|");
     int no_of_pipes = 0;
 
-    while (parts[no_of_pipes] != NULL)
-    {
+    while (parts[no_of_pipes] != NULL) {
         ++no_of_pipes;
         parts[no_of_pipes] = strtok(NULL, "|");
     }
@@ -34,79 +32,23 @@ void redirection(char *args)
     --no_of_pipes;
     parts[no_of_pipes + 1] = 0;
 
-    // char **cmnd = (char **)malloc(400);
-    // int i = 0;
-    // cmnd[0] = strtok(parts[0], " \t");
-    // while (cmnd[i] != NULL)
-    // {
-    //     ++i;
-    //     cmnd[i] = strtok(NULL, " \t");
-    // }
-    // cmnd[i] = 0;
-    // // char **cmnd2 = (char **)malloc(400);
-    // // i = 0;
-    // // cmnd2[0] = strtok(parts[1], " \t");
-    // // while (cmnd2[i] != NULL)
-    // // {
-    // //     ++i;
-    // //     cmnd2[i] = strtok(NULL, " \t");
-    // // }
-    // // cmnd2[i] = 0;
-
-    // // main piping working code
+    // main piping working code
 
     int pipefd[2];
     int pipefd2[2];
-    // pid_t p1;
-
-    // if (pipe(pipefd) < 0)
-    // {
-    //     printf("\nPipe could not be initialized");
-    //     return;
-    // }
-    // p1 = fork();
-    // if (p1 < 0)
-    // {
-    //     printf("\nCould not fork");
-    //     return;
-    // }
-    // if (p1 == 0)
-    // {
-    //     // Child 1 executing..
-    //     // It only needs to write at the write end
-
-    //     // here we need to duplicate it because execvp used below reads from stdin and outputs to stdout but we want to read and write from pipe ends .
-    //     dup2(pipefd[1], STDOUT_FILENO);
-    //     for (int j = 0; j < 2 * no_of_pipes; j++)
-    //     {
-    //         close(pipefd[j]);
-    //     }
-
-    //     if (execvp(cmnd[0], cmnd) < 0)
-    //     {
-    //         printf("\nCould not execute command 1..");
-    //     }
-    //     exit(0);
-    // }
-    // else
-    // {
+    // here we need to duplicate it because execvp used below reads from
+    // stdin and outputs to stdout but we want to read and write from pipe
+    //     ends . dup2(pipefd[1], STDOUT_FILENO); for (int j = 0; j < 2 *
     int cnt = 0, p = 0;
 
-    while (cnt <= no_of_pipes)
-    {
-        // printf("kan2\n");
-        if (cnt % 2 == 0)
-        {
-            if (pipe(pipefd) < 0)
-            {
+    while (cnt <= no_of_pipes) {
+        if (cnt % 2 == 0) {
+            if (pipe(pipefd) < 0) {
                 printf("\nPipe could not be initialized");
                 return;
             }
-        }
-        else
-        {
-            if (pipe(pipefd2) < 0)
-            {
+        } else {
+            if (pipe(pipefd2) < 0) {
                 printf("\nPipe could not be initialized");
                 return;
             }
@@ -117,8 +59,7 @@ void redirection(char *args)
         int j = 0;
 
         ++p;
-        while (cmnd2[j] != NULL)
-        {
+        while (cmnd2[j] != NULL) {
             ++j;
             cmnd2[j] = strtok(NULL, " \t");
         }
@@ -127,97 +68,47 @@ void redirection(char *args)
         // Parent executing
         pid_t p2 = fork();
 
-        if (p2 < 0)
-        {
+        if (p2 < 0) {
             printf("\nCould not fork");
             return;
         }
-        // printf("p%s %s %s\n", cmnd2[0], cmnd2[1], cmnd2[2]);
         // Child 2 executing..
         // It only needs to read at the read end
-        if (p2 == 0)
-        {
-            if (cnt % 2 == 0)
-            {
-                if (cnt != 0)
-                {
+        if (p2 == 0) {
+            if (cnt % 2 == 0) {
+                if (cnt != 0) {
                     dup2(pipefd2[0], STDIN_FILENO);
                 }
-                if (cnt != no_of_pipes)
-                {
+                if (cnt != no_of_pipes) {
                     dup2(pipefd[1], STDOUT_FILENO);
                 }
-            }
-            else
-            {
+            } else {
                 dup2(pipefd[0], STDIN_FILENO);
-                if (cnt != no_of_pipes)
-                {
+                if (cnt != no_of_pipes) {
                     dup2(pipefd2[1], STDOUT_FILENO);
                 }
             }
 
-            // if (cnt != no_of_pipes - 1)
-            // {
-            //     if (cnt % 2 == 0)
-            //     {
-            //         dup2(pipefd[1], STDOUT_FILENO);
-            //     }
-            //     else
-            //     {
-            //         dup2(pipefd[1], STDOUT_FILENO);
-            //     }
-            // }
-            // else
-            // {
-            // }
-            // close(pipefd[0]);
-            // close(pipefd[1]);
-            // for (int j = 0; j < 2 * no_of_pipes; j++)
-            // {
-            // close(pipefd[0]);
-            // close(pipefd[1]);
-            // close(pipefd2[0]);
-            // close(pipefd2[1]);
-            // }
-            // printf("kani\n");
-            // printf("%s\n", cmnd2[0]);
-            if (execvp(cmnd2[0], cmnd2) < 0)
-            {
+            if (execvp(cmnd2[0], cmnd2) < 0) {
                 printf("\nCould not execute command 2..");
             }
             exit(0);
-        }
-        else
-        {
-            if (cnt % 2 == 0)
-            {
-                if (cnt != 0)
-                {
+        } else {
+            if (cnt % 2 == 0) {
+                if (cnt != 0) {
                     close(pipefd2[0]);
                 }
-                if (cnt != no_of_pipes)
-                {
+                if (cnt != no_of_pipes) {
                     close(pipefd[1]);
                 }
-            }
-            else
-            {
-                // dup2(pipefd[0], STDIN_FILENO);
+            } else {
                 close(pipefd[0]);
-                if (cnt != no_of_pipes)
-                {
+                if (cnt != no_of_pipes) {
                     close(pipefd2[1]);
                 }
             }
             cnt += 1;
-            // printf("kanis\n");
-            //continue;
-            // parent executing, waiting for two children
-            // wait(NULL);
             wait(NULL);
-            // exit(0);
         }
     }
-    // }
 }
